519d88d322110bef3d291f174b8f3692200214b4
Ben Owen <ben@hiscamera.com>
2020-07-08 12:29:23 -0500

    Remove default Review functionality

--- assets/js/theme/product.js
+++ assets/js/theme/product.js
@@ -2,7 +2,6 @@
  Import all product specific js
  */
 import PageManager from './page-manager';
-import Review from './product/reviews';
 import collapsibleFactory from './common/collapsible';
 import ProductDetails from './common/product-details';
 import videoGallery from './product/video-gallery';
@@ -12,18 +11,10 @@ export default class Product extends PageManager {
     constructor(context) {
         super(context);
         this.url = window.location.href;
-        this.$reviewLink = $('[data-reveal-id="modal-review-form"]');
         this.$bulkPricingLink = $('[data-reveal-id="modal-bulk-pricing"]');
     }
 
     onReady() {
-        // Listen for foundation modal close events to sanitize URL after review.
-        $(document).on('close.fndtn.reveal', () => {
-            if (this.url.indexOf('#write_review') !== -1 && typeof window.history.replaceState === 'function') {
-                window.history.replaceState(null, document.title, window.location.pathname);
-            }
-        });
-
         let validator;
 
         // Init collapsible
@@ -34,30 +25,7 @@ export default class Product extends PageManager {
 
         videoGallery();
 
-        const $reviewForm = classifyForm('.writeReview-form');
-        const review = new Review($reviewForm);
-
-        $('body').on('click', '[data-reveal-id="modal-review-form"]', () => {
-            validator = review.registerValidation(this.context);
-        });
-
-        $reviewForm.on('submit', () => {
-            if (validator) {
-                validator.performCheck();
-                return validator.areAll('valid');
-            }
-
-            return false;
-        });
-
-        this.productReviewHandler();
         this.bulkPricingHandler();
-    }
-
-    productReviewHandler() {
-        if (this.url.indexOf('#write_review') !== -1) {
-            this.$reviewLink.trigger('click');
-        }
     }
 
     bulkPricingHandler() {
