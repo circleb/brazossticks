d499eaa968e8beb22c7ff9e4f2c5ee9f46064771
Ben Owen <ben@hiscamera.com>
2020-10-07 12:30:50 -0500

    product page fixes

--- assets/js/theme/product.js
+++ assets/js/theme/product.js
@@ -27,8 +27,45 @@ export default class Product extends PageManager {
 
         this.bulkPricingHandler();
 
+        // Auto select the first wood and size option
         $('[data-name="Size"] input:first').trigger( "click" ).change();
         $('[data-name="Wood"] input:first').trigger( "click" ).change();
+
+        // Show the help link only if it's a stick or cane and display the right modal respectively
+        if ($('.radio-option-help').length) {
+            const breadcrumbs = $('.breadcrumbs').text();
+            if (breadcrumbs.includes("Walking Canes")) {
+                $('.radio-option-help').data('reveal-id', 'cane-sizing-modal');
+            } else if (breadcrumbs.includes("Walking Sticks")) {
+                $('.radio-option-help').data('reveal-id', 'stick-sizing-modal');
+            } else {
+                $('.radio-option-help').css('display', 'none');
+            }
+        }
+
+        // Only show the customily canvas if the "Add Engraving" option is set to "Yes"
+        window.addEventListener('load', function() {
+            var engravingInputYes = document.querySelectorAll('[data-name="Add Engraving"] input')[0];
+            var engravingInputNo = document.querySelectorAll('[data-name="Add Engraving"] input')[1];
+
+            var displayCustomilyFunction = window.displayCustomilyCanvas;
+            function displayCustomilyCanvasCustom() {
+                if(!engravingInputYes.checked) return;
+                displayCustomilyFunction();
+            }
+
+            window.displayCustomilyCanvas = displayCustomilyCanvasCustom;
+
+            engravingInputYes.addEventListener('click', function() {
+                $('.productView-thumbnails').hide();
+                window.displayCustomilyCanvas();
+            });
+
+            engravingInputNo.addEventListener('click', function() {
+                $('.productView-thumbnails').show();
+                window.hideCustomilyCanvas();
+            });
+        })
     }
 
     bulkPricingHandler() {
